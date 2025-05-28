2f3debb6a5e32804a0b10a9232cce952216a3a32
Niariasve <niarias@espol.edu.ec>
2025-05-27 22:54:34 -0500

    ✨ Feature Appointments
    fixes #10

--- src/app/(gastrify)/appointments/page.tsx
+++ src/app/(gastrify)/appointments/page.tsx
@@ -1,19 +1,5 @@
-import type { Metadata } from "next";
+import BigCalendar from "@/features/appointments/components/big-calendar";
 
-import { TypographyH1 } from "@/shared/components/ui/typography";
-
-import { TestCalendar } from "@/features/appointments/components/test-calendar";
-
-export const metadata: Metadata = {
-  title: "Gastrify | Appointments",
-};
-
-export default function ChatPage() {
-  return (
-    <main className="flex flex-col gap-6">
-      <TypographyH1>Appointments</TypographyH1>
-
-      <TestCalendar />
-    </main>
-  );
+export default function Page() {
+  return <BigCalendar />;
 }
